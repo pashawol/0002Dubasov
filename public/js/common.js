1468a0f915d3c28434db3aafbf687aad91135766
Pavel <wol1414@gmail.com>
2020-01-16 21:27:23 +0300

    add form and animate too dots

--- public/js/common.js
+++ public/js/common.js
@@ -1,31 +1,54 @@
 "use strict";
 
-var JSCCommon = {
-	inputMask: function inputMask() {
-		// mask for input
-		$('input[type="tel"]').attr("pattern", "[+][0-9]{1}[(][0-9]{3}[)][0-9]{3}-[0-9]{2}-[0-9]{2}").inputmask("+9(999)999-99-99");
-	} // /inputMask
-
+var JSCCommon = {// inputMask() {
+	// 	// mask for input
+	// 	$('input[type="tel"]').attr("pattern", "[+][0-9]{1}[(][0-9]{3}[)][0-9]{3}-[0-9]{2}-[0-9]{2}").inputmask("+9(999)999-99-99");
+	// }
+	// /inputMask
 };
 
 function eventHandler() {
 	// для свг
-	svg4everybody({});
-	JSCCommon.inputMask(); // JSCCommon.CustomInputFile();
+	svg4everybody({}); // JSCCommon.inputMask();
+	// JSCCommon.CustomInputFile();
 	// добавляет подложку для pixel perfect
-
-	$(".main-wrapper").after('<div class="screen" style="background-image: url(screen/main.jpg);"></div>'); // /добавляет подложку для pixel perfect
+	// $(".main-wrapper").after('<div class="screen" style="background-image: url(screen/main.jpg);"></div>')
+	// /добавляет подложку для pixel perfect
 
 	$('#fullpage').fullpage({
 		//options here
 		autoScrolling: true,
 		// scrollHorizontally: true,
 		navigation: true,
-		scrollOverflow: true // scrollBar: true,
+		scrollOverflow: true,
+		// scrollBar: true,
 		// autoScrolling: true,
 		// scrollOverflow: true,
 		// fixedElements: '.top-line',
+		responsiveWidth: 992,
+		responsiveHeight: 550 // normalScrollElements: '.s-form',
 
+	});
+	$(document).on('click', ".show-form-wrap", function () {
+		$(this).text($(this).data("title"));
+		$('.s-form-nav__dropdown').slideToggle();
+	});
+	$(document).on('click', ".hide-form-wrap", function () {
+		var _this = this;
+
+		$(this).text($(this).data("title"));
+		setTimeout(function () {
+			$(_this).removeClass("hide-form-wrap").addClass('show-form-wrap');
+		}, 1000);
+		$('.form-wrap').fadeOut(function () {
+			$('.s-form-nav__dropdown').slideDown();
+		});
+	});
+	$(".s-form-nav__dropdown-item").click(function () {
+		$('.form-wrap').slideDown();
+		$('.s-form-nav__dropdown').slideUp();
+		$(".s-form-nav__title").removeClass('show-form-wrap').addClass("hide-form-wrap").text($(this).data("title"));
+		$(".order").val($(this).data("title"));
 	});
 }
 
--- sourse/js/common.js
+++ sourse/js/common.js
@@ -1,9 +1,9 @@
 const JSCCommon = {
 
-	inputMask() {
-		// mask for input
-		$('input[type="tel"]').attr("pattern", "[+][0-9]{1}[(][0-9]{3}[)][0-9]{3}-[0-9]{2}-[0-9]{2}").inputmask("+9(999)999-99-99");
-	}
+	// inputMask() {
+	// 	// mask for input
+	// 	$('input[type="tel"]').attr("pattern", "[+][0-9]{1}[(][0-9]{3}[)][0-9]{3}-[0-9]{2}-[0-9]{2}").inputmask("+9(999)999-99-99");
+	// }
 	// /inputMask
 
 };
@@ -14,11 +14,11 @@ function eventHandler() {
 	svg4everybody({});
 
 
-	JSCCommon.inputMask();
+	// JSCCommon.inputMask();
 
 	// JSCCommon.CustomInputFile();
 	// добавляет подложку для pixel perfect
-	$(".main-wrapper").after('<div class="screen" style="background-image: url(screen/main.jpg);"></div>')
+	// $(".main-wrapper").after('<div class="screen" style="background-image: url(screen/main.jpg);"></div>')
 	// /добавляет подложку для pixel perfect
 
 
@@ -32,7 +32,35 @@ function eventHandler() {
 		// autoScrolling: true,
 		// scrollOverflow: true,
 		// fixedElements: '.top-line',
+		responsiveWidth: 992,
+		responsiveHeight: 550,
+		// normalScrollElements: '.s-form',
 	});
+
+	$(document).on('click', ".show-form-wrap", function () {
+		$(this).text($(this).data("title"))
+		$('.s-form-nav__dropdown').slideToggle();
+	})
+
+	$(document).on('click', ".hide-form-wrap", function () {
+		$(this).text($(this).data("title"))
+		setTimeout(() => {
+			$(this).removeClass("hide-form-wrap").addClass('show-form-wrap')
+		}, 1000);
+
+		$('.form-wrap').fadeOut(function () {
+			$('.s-form-nav__dropdown').slideDown();
+		});
+
+	})
+
+	$(".s-form-nav__dropdown-item").click(function () {
+		$('.form-wrap').slideDown();
+		$('.s-form-nav__dropdown').slideUp();
+		$(".s-form-nav__title").removeClass('show-form-wrap').addClass("hide-form-wrap").text($(this).data("title"))
+		$(".order").val($(this).data("title"));
+
+	})
 };
 if (document.readyState !== 'loading') {
 	eventHandler();
